b93c9209fec68ac274f3c7b9598fb7caf7df3ffc
Tierra Byrd <tierrabyrd1@gmail.com>
2024-06-26 13:33:37 -0500

    update about and landing page

---
+++ src/components/Sports/SingleSports.jsx
@@ -0,0 +1,10 @@
+const SingleSports = () => {
+    return (
+      <div>
+        <h2>Single Sports</h2>
+   
+      </div>
+    );
+  }
+  
+  export default SingleSports;
---
+++ src/components/Sports/TeamSports.jsx
@@ -0,0 +1,10 @@
+const TeamSports = () => {
+    return (
+      <div>
+        <h2>Team Sports</h2>
+        
+      </div>
+    );
+  }
+  
+  export default TeamSports;
